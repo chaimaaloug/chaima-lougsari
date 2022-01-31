8ccb574e45ac031d407e83b54cd3e08351f1ab95
Chaimaa Lougsari <lougsari@outlook.com>
2022-01-31 09:42:23 +0100

    commit

--- src/index.php
+++ src/index.php
@@ -2,5 +2,5 @@
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 10 janvier 2022 </p>';
-?>
 
+echo 'Hello chaima';
